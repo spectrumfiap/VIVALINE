4f1aaa17785c3a40a195f9d0431cab3c8e8833f1
Luannoq7 <klochko679@gmail.com>
2025-03-19 00:46:59 -0300

    Terminado a page de ChatBot

--- Sprint 3_Front End/vivalinesprint/src/app/Chatbot/page.tsx
+++ Sprint 3_Front End/vivalinesprint/src/app/Chatbot/page.tsx
@@ -1,7 +1,28 @@
+import Image from 'next/image'
 import React from 'react'
 const Chatbot = () => {
   return (
-    <div>Chatbot</div>
+    <main className="flex-1">
+  <div className="relative bg-cover bg-center h-full pt-10 pl-10 [background-image:url('/assets/metro-barrafunda.png')]">
+    
+    {/* Overlay escuro separado */}
+    <div className="absolute inset-0 bg-black opacity-60"></div>
+
+    {/* Conteúdo acima da overlay */}
+    <div className="relative z-10 h-full">
+      <Image src="/assets/VivaChat.svg" width={100} height={100} alt="vivavchat" />
+
+      <div className="flex flex-col justify-center items-center h-full pb-90 gap-10">
+        <h1 className="text-white text-5xl font-bold  ">Como posso ajudar?</h1>
+        <input className="border-2 rounded-4xl pl-20 mt-6 
+            ml-2 border-none bg-white p-5 text-2xl text-black w-[50%] gap-1 [background-image:url('/assets/chat_bubble_search.svg')] bg-no-repeat  bg-[position:30px_22px] " placeholder="Digite sua dúvida" />
+      </div>
+
+    </div>
+
+  </div>
+</main>
+
   )
 }
 export default Chatbot
--- Sprint 3_Front End/vivalinesprint/src/components/Header/index.tsx
+++ Sprint 3_Front End/vivalinesprint/src/components/Header/index.tsx
@@ -3,7 +3,8 @@ import Link from "next/link"
 
 const Header = () => {
     return(
-        <header className="bg-[#C7CDCF] flex gap-40 items-center pb-4 border-b-1" >
+        <header className="bg-[#C7CDCF] flex items-center pb-4 justify-between pr-8 border-b-1" >
+
             <Image src="/assets/VIVALINE.svg" alt="Logo" width={200} height={100} className="pb-0 pt-5 pl-6 pr-0"/>
             <input  className="border-2 rounded-2xl pl-3 mt-6 w-60
             ml-2  border-gray-500 bg-[#D9D9D9] p-1.5   text-black" placeholder="O que você deseja?" type="text"/>
@@ -13,7 +14,7 @@ const Header = () => {
                    shadow-md hover:shadow-lg">Pesquisar</button>
 
 
-                <ul className="flex gap-32 items-center pt-6 ">
+                <ul className="flex items-center pt-6 gap-16">
                     <li className="text-2xl">
                         <Link href="/QuemSomos" target="_blank">Quem somos?
                         </Link>
